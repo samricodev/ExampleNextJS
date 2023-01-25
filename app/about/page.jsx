2ca54dd06660d50beb937ef6c08e2e2d74c303b0
samricodev <samuel.isaac.rico.estrada@gmail.com>
2023-01-25 00:23:06 -0600

    intial next project

---
+++ app/about/page.jsx
@@ -0,0 +1,5 @@
+export default function AboutPage () {
+  return (
+    <h1>This is about page</h1>
+  )
+}
---
+++ app/components/Navigation.jsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import styles from './Navigation.module.css'
+
+const links = [
+  {
+    label: 'Home',
+    route: '/'
+  },
+  {
+    label: 'About',
+    route: '/about'
+  }
+]
+
+export default function Navigation () {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul className={styles.navigation}>
+          {links.map(({ label, route }) => {
+            return <li key={route}><Link href={route}>{label}</Link></li>
+          })}
+        </ul>
+      </nav>
+    </header>
+  )
+}
